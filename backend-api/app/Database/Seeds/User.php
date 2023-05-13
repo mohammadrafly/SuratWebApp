9b254c495bca047c11786c02076b58dba62ede49
Mohammad Rafly <mohammadrafly19@gmail.com>
2023-05-13 22:38:34 +0700

    minor change #1

--- backend-api/app/Database/Seeds/User.php
+++ backend-api/app/Database/Seeds/User.php
@@ -8,12 +8,28 @@ class User extends Seeder
 {
     public function run()
     {
-        $data = [
-            'email'    => 'admin@gmail.com',
-            'password' => password_hash('admin', PASSWORD_DEFAULT),
-            'role'     => 'admin',
-            'name'     => 'admin',
-        ];
-        $this->db->table('users')->insert($data);
+        $data = array(
+            array(
+                'email'    => 'admin@gmail.com',
+                'password' => password_hash('admin', PASSWORD_DEFAULT),
+                'role'     => 'admin',
+                'name'     => 'admin',
+            ),
+            array(
+                'email'    => 'warga@gmail.com',
+                'password' => password_hash('warga', PASSWORD_DEFAULT),
+                'role'     => 'warga',
+                'name'     => 'Ahmad Kriptokurensi',
+            ),
+            array(
+                'email'    => 'kepaladesa@gmail.com',
+                'password' => password_hash('kepaladesa', PASSWORD_DEFAULT),
+                'role'     => 'kepala_desa',
+                'name'     => 'Bung Tomo',
+            ),
+        );
+        
+        $this->db->table('users')->insertBatch($data);
+        
     }
 }
